83bee311f34f43e5b769f7d6e18c2d0c8bfa6b5a
kitaro-tn <tora.1986.tatsu@gmail.com>
2016-06-19 00:50:28 +0900

    add slack post message

--- lib/git-pulls-controll/slack.py
+++ lib/git-pulls-controll/slack.py
@@ -1,19 +1,30 @@
 # coding: utf-8
 
-import httplib, json, os
+import httplib, urllib, json, os
+
+def post_message(message):
+  token = "slack-token"
+  channel_id = "channel_id"
+  conn = httplib.HTTPSConnection("slack.com")
+  params = urllib.urlencode({ "token" : token, "channel": channel_id, "text": message, "pretty": 1 })
+  conn.request("GET", "/api/chat.postMessage?%s" % params)
+  response = conn.getresponse()
+  data = json.loads(response.read())
+  print data
 
 def check_branch(data, base_branch_name, allow_branch_name):
   if data[0]["base"]["ref"] == base_branch_name:
     if data[0]["head"]["ref"] != allow_branch_name:
-      print data[0]["head"]["ref"]
-      print "not required!"
+      message = { "base_branch": data[0]["head"]["ref"], "head_branch": data[0]["head"]["ref"], "message" : "not required!" }
     else:
-      print "ok"
+      message = { "base_branch": data[0]["head"]["ref"], "head_branch": data[0]["head"]["ref"], "message" : "ok" }
+
+    post_message(json.dumps(message, indent=2))
 
 def main():
   conn = httplib.HTTPSConnection("api.github.com")
   headers = {"Authorization": "token %s" % os.getenv("GITHUB_TOKEN"), "User-Agent": ""}
-  conn.request("GET", "/repos/kitaro-tn/git-flow-study/pulls?sort=updated&direction=desc", headers=headers)
+  conn.request("GET", "/repos/:owner/:repo/pulls?sort=updated&direction=desc", headers=headers)
   response = conn.getresponse()
   data = json.loads(response.read())
   check_branch(data, "master", "develop")
